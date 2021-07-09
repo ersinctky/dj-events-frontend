1216c072975c19658f1b3e8dac2f7241f9d6bbf8
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-09 18:28:26 +0300

    Pages & Routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an App to find the lastest DJ and other musical events</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>Add Events</h1>
+    </div>
+  );
+}
