9078ae49c805dfa2ac92915c7c5f8d897ce6b2b9
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-09 20:36:02 +0300

    Header & Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
 export default function Layout({ title, description, children, keywords }) {
@@ -9,7 +11,9 @@ export default function Layout({ title, description, children, keywords }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Foote />
     </div>
   );
 }
