7ffabebdc93abcb1ce08e81a77a594d0a9bfb9c7
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-11 13:58:51 +0300

    getServerSideProps & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
