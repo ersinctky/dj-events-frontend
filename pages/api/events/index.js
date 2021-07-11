c829796aaae2a750ce1adb036026a0a4fe7527e7
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-11 15:44:20 +0300

    Displaying Events

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,10 +1,26 @@
-const { events } = require("./data.json");
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function (req, res) {
-  if (req.method === "GET") {
-    res.status(200).json(events);
-  } else {
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).json({ message: `Method ${req.method} is not allowed` });
-  }
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import Link from "next/link";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -14,7 +27,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
