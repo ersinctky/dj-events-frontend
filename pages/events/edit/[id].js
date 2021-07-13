de5682a56d83fb4669d00a0a300160756859ca9c
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-13 14:43:45 +0300

    Image Upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -8,6 +8,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import Modal from "@/components/Modal";
+import ImageUpload from "@/components/ImageUpload";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 
@@ -24,7 +25,6 @@ export default function EditEventPage({ evt }) {
   const [imagePreview, setImagePreview] = useState(
     evt.image ? evt.image.formats.thumbnail.url : null
   );
-
   const [showModal, setShowModal] = useState(false);
 
   const router = useRouter();
@@ -60,6 +60,13 @@ export default function EditEventPage({ evt }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+  };
+
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   };
 
   return (
@@ -162,8 +169,9 @@ export default function EditEventPage({ evt }) {
           <FaImage /> Set Image
         </button>
       </div>
+
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        Image Upload
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   );
