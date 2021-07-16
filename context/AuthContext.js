2d81cd912826f8c31433d21058f04f376da9dab2
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-16 15:15:32 +0300

    Persist Logged In User

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
   // Register user
   const register = async (user) => {
     console.log(user);
@@ -30,6 +34,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user);
+      router.push("/account/dashboard");
     } else {
       setError(data.message);
       setError(null);
@@ -43,7 +48,14 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log("Check");
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
