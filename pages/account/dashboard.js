ca30c7602a3f274b8a9cfc381660969c31c1004e
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-17 14:34:54 +0300

    Getting User Events For Dashboard

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,32 @@
+import { parseCookies } from "@/helpers/index";
+import { API_URL } from "@/config/index";
 import Layout from "@/components/Layout";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
   return (
     <Layout title="User Dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  console.log(token);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
