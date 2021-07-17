f2121369fbae0ff5d2eac3a0fec5a40794e4274f
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-17 14:57:52 +0300

    Display Dashboard Events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,19 +1,30 @@
 import { parseCookies } from "@/helpers/index";
-import { API_URL } from "@/config/index";
 import Layout from "@/components/Layout";
+import DashboardEvent from "@/components/DashboardEvent";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Dashboard.module.css";
 
 export default function DashboardPage({ events }) {
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ req }) {
   const { token } = parseCookies(req);
-
-  console.log(token);
 
   const res = await fetch(`${API_URL}/events/me`, {
     method: "GET",
