9353311b01562b71a72bb0d7f76eb0fb78a64099
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-07-25 14:52:59 +0300

    cors settings updated

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "https://djeventsbackendersin.herokuapp.com",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
